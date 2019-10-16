b4d334d2d222897b5ddba2105be8c14e0e555669
juan pablo <iarnaudojuanpablo@gmail.com>
2019-10-16 15:30:19 -0300

    agregado login

--- app/src/main/java/com/example/loginactivity/LoginActivity.java
+++ app/src/main/java/com/example/loginactivity/LoginActivity.java
@@ -6,15 +6,38 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 
 public class LoginActivity extends AppCompatActivity {
 
-    private Button btnCrearCuenta;
+    private EditText etEmail, etPassword;
+    private Button btnCrearCuenta, btnLogear;
+
+
+    FirebaseAuth mAuth;
+    DatabaseReference mDatabase;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+        mAuth = FirebaseAuth.getInstance();
+        mDatabase = FirebaseDatabase.getInstance().getReference();
+
+        etEmail = findViewById(R.id.etLoginEmail);
+        etPassword = findViewById(R.id.etLoginPassword);
+        btnLogear = findViewById(R.id.btnIngresar);
+        btnLogear.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
+
 
         btnCrearCuenta = findViewById(R.id.btnCrearCuenta);
         btnCrearCuenta.setOnClickListener(new View.OnClickListener() {
@@ -25,4 +48,6 @@ public class LoginActivity extends AppCompatActivity {
             }
         });
     }
+
+
 }
